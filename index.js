d1f48df7b2a8709fd5cbef356be6176a11325110
zmpAnas <150332884+zmpAnas@users.noreply.github.com>
2023-11-11 23:31:53 +0500

    i added html,css,js

---
+++ index.js
@@ -0,0 +1,46 @@
+const startE1 = document.getElementById("start");
+const stopE1 = document.getElementById("stop");
+const resetE1 = document.getElementById("reset");
+const timerE1 = document.getElementById("timer");
+
+let interval
+let timeLeft = 1500;
+
+function updateTimer(){
+    let minutes= Math.floor(timeLeft / 60)
+    let seconds = timeLeft % 60;
+    let formattedTime = `${minutes.toString().padStart(2, "0")} : ${seconds.toString().padStart(2 , "0")}`;
+
+    timerE1.innerHTML = formattedTime;
+
+}
+function startTimer() {
+    interval = setInterval(()=>{
+        timeLeft--;
+        updateTimer();
+        if(timeLeft === 0){
+            clearInterval(interval);
+            alert("Time's up");
+            timeLeft = 1500;
+            updateTimer();
+        }
+    },1000)
+}
+// function startTimer(){
+//     console.log("start");
+// }
+
+function stopTimer(){
+    clearInterval(interval);
+}
+
+function resetTimer(){
+    clearInterval(interval);
+    timeLeft = 1500;
+    updateTimer();
+}
+
+startE1.addEventListener("click", startTimer);
+stopE1.addEventListener("click", stopTimer);
+resetE1.addEventListener("click", resetTimer);
+
